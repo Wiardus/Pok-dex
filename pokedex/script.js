3c408c3ffb503e45144eb5f3d4cd4b8d254b3e39
Wiardus <ynte.braaksma@student.nhlstenden.com>
2021-08-11 10:30:28 +0200

    Added connection with PokeApi and delete button
    Created a searchPokemon function that displays the name of a pokemon on the right display of the pokedex by requesting data from the PokeApi when pressing the button that has onclick = searchPokemon and based on the inputvalue that is changed to type = text. Also added the PokeApi link in the HTML file. To clear the result that is displayed with the searchPokemon button, I created a delete function on the green button.

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,3 +1,32 @@
+function searchPokemon() {
+    const P = new Pokedex.Pokedex()
+
+    P.getPokemonByName(document.getElementById('input').value)
+    .then(function(response) {
+        console.log(response)
+
+        const displayRight = document.getElementById('disRight')
+        let pokemonName = document.createElement('p')
+
+        pokemonName.innerHTML = "Name: " + response.name
+        pokemonName.setAttribute('id', 'pokemonName')
+        displayRight.appendChild(pokemonName)
+        
+
+       //const display2 = document.getElementById('display2')
+       // display2.innerHTML = response.sprites.front_default
+        
+    }) 
+}   
+
+const remove = () => {
+    pokemonName.remove();
+}
+
+
+
+
+
 
 let bulbasaur = {
     name: 'Bulbasaur',
@@ -24,7 +53,7 @@ let venusaur = {
 }
 
 // displays the properties of the Pokémon on the screen
-function displayName() {
+/* function displayName() {
     let inputValue = input.value;
     if (inputValue == 1) {
         document.getElementById("disRight").innerHTML = Object.values(bulbasaur);
@@ -33,4 +62,17 @@ function displayName() {
     } else if (inputValue == 3) {
         document.getElementById("disRight").innerHTML = Object.values(venusaur);
     }  
-}
+} */
+
+
+
+/* var inputValue = input.value;
+
+if (inputValue == 1) {
+    for (const [key, value] of Object.entries(bulbasaur)) {
+        var element = document.createElement('p');
+        element.setAttribute('id', key);
+        element.innerHTML = $;{key}:$;{value};
+        document.getElementById('disRight').appendChild(element);
+    }   
+} */
